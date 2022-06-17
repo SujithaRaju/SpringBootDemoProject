781db9594a5b0b3e0b481edce953c56ecc17d12c
SujithaRaju <SujithaRaju4@gmail.com>
2022-06-17 17:02:19 -0400

    Added Swagger UI

--- src/main/java/com/example/SpringBootDemoProject/SpringBootDemoProjectApplication.java
+++ src/main/java/com/example/SpringBootDemoProject/SpringBootDemoProjectApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {
